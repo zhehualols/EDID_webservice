9a754545e3e62381056c5eed2a021c56268d1fed
zhehualols <zhehuajin@live.com.sg>
2016-07-15 17:18:25 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cherrypy
+import numpy
 import mysql.connector
 import os.path
 import json
@@ -26,15 +27,22 @@ class Connection():
         return result
     def header_loader(self):
         query = 'show columns from edid_semantics'
-        print query
         self.Cursor.execute(query)
         result = [column[0] for column in self.Cursor.fetchall()]
         self.Connection.commit()
         return result
     
 class Json():
-    def header_json(self):
-        pass
+    def json_loader(self, table_header , table_data):
+        data = numpy.array(table_data)
+        b = json.dumps(table_data)
+        print b 
+        print len(data)
+        with open('data.json' , 'w') as outfile:
+            json.dump("[",outfile,indent=1)
+            for a in range(0,len(data)):
+                for i in range(0,9):
+                    json.dump('"'+table_header[i]+'"'+':'+'"'+data[a,i]+'"', outfile)
 class Display_table():
     def __inti__(self , data):
         pass
@@ -49,16 +57,20 @@ if __name__=="__main__":
                 'tools.staticdir.dir':'./public'
         }
     }
-    cherrypy.config.update({'server.socket_port': 9090})
-    print os.getcwd()
     webapp = table_loader()
     mysql = Connection('172.16.170.136','root','password123!','hdmi')
     table_data = mysql.data_loader()
-    print table_data
     table_header = mysql.header_loader()
+    print "table header :" 
     print table_header
+    print "table data :" 
+    print table_data
+    Format_json = Json()
+    output = Format_json.json_loader(table_header, table_data)
     display = Display_table()
-    #cherrypy.quickstart(webapp,'/', conf)
+    cherrypy.config.update({'server.socket_port': 9090})
+    cherrypy.server.socket_host = '172.16.170.164'
+    cherrypy.quickstart(webapp,'/', conf)
 #     cherrypy.tree.mount(webapp,'/', conf)
 #     cherrypy.engine.start()
     
