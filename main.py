05d0302d1f46221ab8fc79aa43ada2f1ee0cf4f9
zhehualols <zhehuajin@live.com.sg>
2016-07-21 15:34:26 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import cherrypy
-import numpy
 import mysql.connector
 import os.path
 import json
+from timeit import itertools
 
 class table_loader(object):
     @cherrypy.expose
     def index(self):
-        return open('/home/development/workspace/Edid_test/public/html/index.html')
+        return open('/home/development/workspace/Edid_test/index.html')
 class Connection():
     def __init__(self,host,user,password,db):
         self.host = host
@@ -20,11 +20,13 @@ class Connection():
         print 'Database connected'
     
     def data_loader(self):
+        #query = 'select concat("[", group_concat( concat("{id:'",id,"'"),concat(",prd_manufacturer_name:'",prd_manufacturer_name,"'"),concat(",prd_code:'",prd_code,"'"),concat(",prd_serial_no:'",prd_serial_no,"'"),concat(",prd_week_of_manufacture:'",prd_week_of_manufacture,"'"),concat(",prd_year_of_manufacture:'",prd_year_of_manufacture,"'"),concat(",str_version_no:'",str_version_no,"'"),concat(",str_revision_no:'",str_revision_no,"'"),concat(",monitor_name_tag:'",monitor_name_tag,"'}")),"]") as json from edid_semantics'
         query = 'select * from edid_semantics'
+        print 'query:' + query
         self.Cursor.execute(query)
-        result = self.Cursor.fetchall()
-        self.Connection.commit()
-        return result
+        desc = self.Cursor.description
+        return [dict(itertools.izip([col[0] for col in desc], row))
+                for row in self.Cursor.fetchall()]
     def header_loader(self):
         query = 'show columns from edid_semantics'
         self.Cursor.execute(query)
@@ -34,18 +36,8 @@ class Connection():
     
 class Json():
     def json_loader(self, table_header , table_data):
-        data = numpy.array(table_data)
-        b = json.dumps(table_data)
-        print b 
-        print len(data)
         with open('data.json' , 'w') as outfile:
-            json.dump("[",outfile,indent=1)
-            for a in range(0,len(data)):
-                for i in range(0,9):
-                    json.dump('"'+table_header[i]+'"'+':'+'"'+data[a,i]+'"', outfile)
-class Display_table():
-    def __inti__(self , data):
-        pass
+            json.dump(table_data, outfile)
         
 if __name__=="__main__":
     conf = {
@@ -60,17 +52,14 @@ if __name__=="__main__":
     webapp = table_loader()
     mysql = Connection('172.16.170.136','root','password123!','hdmi')
     table_data = mysql.data_loader()
-    table_header = mysql.header_loader()
-    print "table header :" 
-    print table_header
-    print "table data :" 
     print table_data
+    table_header = mysql.header_loader()
     Format_json = Json()
     output = Format_json.json_loader(table_header, table_data)
-    display = Display_table()
-    cherrypy.config.update({'server.socket_port': 9090})
-    cherrypy.server.socket_host = '172.16.170.164'
-    cherrypy.quickstart(webapp,'/', conf)
+
+#     cherrypy.config.update({'server.socket_port': 9090})
+#     cherrypy.server.socket_host = '172.16.170.175'
+#     cherrypy.quickstart(webapp,'/', conf)
 #     cherrypy.tree.mount(webapp,'/', conf)
 #     cherrypy.engine.start()
     
